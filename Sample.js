23f19f64c5650e37b8ff3d0818dd8ea1215efe25
Naguru Naveen Kumar Reddy <naveenkumarreddynaguru@gmail.com>
2024-11-11 12:25:31 +0530

    Update Sample.js

--- Sample.js
+++ Sample.js
@@ -10,3 +10,55 @@ db.collectionName.find({
     ]
   }
 })
+
+
+import React, { Component } from 'react';
+
+class DatePickerComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dateInput: '',
+            isValidDate: true,
+        };
+    }
+
+    // Function to validate date format (MM/DD/YYYY)
+    validateDateFormat = (dateString) => {
+        // Define the expected date format (MM/DD/YYYY)
+        const datePattern = /^(0[1-9]|1[0-2])\/(0[1-9]|[12][0-9]|3[01])\/\d{4}$/;
+        
+        // Check if the date matches the expected format
+        return datePattern.test(dateString);
+    };
+
+    // Handler for input change
+    handleDateChange = (event) => {
+        const dateInput = event.target.value;
+        const isValidDate = this.validateDateFormat(dateInput);
+
+        // Update state with the input and its validity
+        this.setState({
+            dateInput,
+            isValidDate,
+        });
+    };
+
+    render() {
+        const { dateInput, isValidDate } = this.state;
+
+        return (
+            <div>
+                <input
+                    type="text"
+                    value={dateInput}
+                    onChange={this.handleDateChange}
+                    placeholder="MM/DD/YYYY"
+                />
+                {!isValidDate && <p style={{ color: 'red' }}>Invalid date format. Use MM/DD/YYYY.</p>}
+            </div>
+        );
+    }
+}
+
+export default DatePickerComponent;
